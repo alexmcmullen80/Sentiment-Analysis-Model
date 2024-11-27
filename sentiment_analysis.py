3a37e02b3c865ecd5588e6fbe1638d9cea04aca1
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-11-27 12:07:09 -0500

    change preprocess to return train and test set

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,14 +2,11 @@ import pandas as pd
 
 from preprocess import preprocess
 
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
-vectors, score = preprocess()
-
-X_train, X_test, y_train, y_test = train_test_split(vectors, score, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test  = preprocess(test_size = 0.2)
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
