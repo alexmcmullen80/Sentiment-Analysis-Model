d9143d29cef84bc7dafecaf5e4ae5e71a3bf7276
ArruranK <arrurankamal@gmail.com>
2024-12-03 14:09:55 -0500

    update svm code to use cross validate

--- svm.py
+++ svm.py
@@ -1,8 +1,8 @@
 from sklearn import svm
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.utils import shuffle
 from preprocess import preprocess
+from cross_validate import cross_validate
 
 class SVMClassifier:
     def __init__(self, learning_rate=0.001, c_value=0.001, epoch=1000):
@@ -45,28 +45,12 @@ class SVMClassifier:
 # Load data
 X_train, X_test, y_train, y_test = preprocess(test_size=0.2)
 
-#y_train = np.where(y_train == 0, -1, 1)
-
 C = 1
-learning_rate = 0.0001
+learning_rate = 0.0005
 epoch = 100
 
 # Create SVM classifier
 svm_classifier = SVMClassifier(learning_rate=learning_rate,epoch=epoch, c_value=C)
 
-# Train model
-svm_classifier.fit(X_train, y_train)
-
-# Make predictions
-y_pred = svm_classifier.predict(X_test)
-
-# Evaluate performance
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='weighted')
-recall = recall_score(y_test, y_pred, average='weighted')
-f1 = f1_score(y_test, y_pred, average='weighted')
-
-print(f"Accuracy: {accuracy:.3f}")
-print(f"Precision: {precision:.3f}")
-print(f"Recall: {recall:.3f}")
-print(f"F1 score: {f1:.3f}")
+# train model and cross validate
+cross_validate(X_train, X_test, y_train, y_test, svm_classifier, num_folds=10)
