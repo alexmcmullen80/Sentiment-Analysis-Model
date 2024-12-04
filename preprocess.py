80f2f5f79c9265fd880df95c08ae9bc302dec98e
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-12-04 12:06:51 -0500

    comments and stuff

--- preprocess.py
+++ preprocess.py
@@ -2,23 +2,22 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
-#nltk.download('wordnet')
-#nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('stopwords')
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
 
-# Stopword removal, converting uppercase into lower case, and lemmatization
 def preprocess(test_size=0.2, technique = 'none', percentile = 0):
 
     stopwords = nltk.corpus.stopwords.words('english')
     lemmatizer = WordNetLemmatizer()
     response = []
     score = []
-    files = ['amazon_cells_labelled.txt', 'imdb_labelled.txt', 'yelp_labelled.txt']
 
+    # parse sentences and scores from txt files
+    files = ['amazon_cells_labelled.txt', 'imdb_labelled.txt', 'yelp_labelled.txt']
     for file in files:
         with open('sentiment_labelled_sentences/' + file, 'r') as f:
             lines = f.readlines()
@@ -27,11 +26,16 @@ def preprocess(test_size=0.2, technique = 'none', percentile = 0):
                 response.append(temp[0])
                 score.append(temp[1].strip())
 
+    # sentences preprocessing
     data_without_stopwords = []
     for i in range(0, len(response)):
+
+        # convert uppercase into lowercase
         doc = re.sub('[^a-zA-Z]', ' ', response[i])
         doc = doc.lower()
         doc = doc.split()
+
+        # stopword removal and lemmatization
         doc = [lemmatizer.lemmatize(word) for word in doc if not word in set(stopwords)]
         doc = ' '.join(doc)
         data_without_stopwords.append(doc)
@@ -39,51 +43,28 @@ def preprocess(test_size=0.2, technique = 'none', percentile = 0):
     data = pd.DataFrame(list(zip(data_without_stopwords, score))) 
     data.columns = ['response', 'score']
 
+    # vectorization
     label_encoder = LabelEncoder()
     encoded_labels = label_encoder.fit_transform(score) 
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform(data_without_stopwords).toarray() 
 
+    # additional preprocess techniques given 'technique'
+    # removing reviews with the lowest tf-idf sum (low information content)
     if technique == 'information content':
-        #--------------------------------------------------------------#
-        #removing reviews with the lowest tf-idf sum (low information content)
         vectors = vectorizer.fit_transform(data_without_stopwords).toarray()
-        document_sums = np.sum(vectors, axis=1)  # Use toarray() if sparse
-        threshold = np.percentile(document_sums, percentile)  # Remove the lowest 10%
+        document_sums = np.sum(vectors, axis=1)
+        threshold = np.percentile(document_sums, percentile)
         vectors = vectors[document_sums > threshold]
         encoded_labels = np.array(encoded_labels)[document_sums > threshold]
-        #--------------------------------------------------------------#
+    
+    #removing reviews with low tf-idf variance
     elif technique == 'tf-idf variance':
-        #--------------------------------------------------------------#
-        #removing reviews with low tf-idf variance
         vectors = vectorizer.fit_transform(data_without_stopwords).toarray()
-        document_variances = np.var(vectors, axis=1)  # Use toarray() if sparse
-        threshold = np.percentile(document_variances, percentile)  # Remove the lowest 10%
+        document_variances = np.var(vectors, axis=1)
+        threshold = np.percentile(document_variances, percentile)
         vectors = vectors[document_variances > threshold]
         encoded_labels = np.array(encoded_labels)[document_variances > threshold]
-        #--------------------------------------------------------------#
-    elif technique == 'cosine similarity':
-        #--------------------------------------------------------------#
-        #remove reviews with low cosine similarity to others (outliers)
-        all_vectors = vectorizer.fit_transform(data_without_stopwords)
-        threshold = 1/100 * percentile
-        similarity_matrix = cosine_similarity(all_vectors)
-        mean_similarities = np.mean(similarity_matrix, axis=1)
 
-        filtered_data_and_labels = [
-            (data_without_stopwords[i], score[i]) 
-            for i in range(len(data_without_stopwords)) 
-            if mean_similarities[i] > threshold
-        ]
-
-        # Separate the filtered data and labels
-        data_without_stopwords, filtered_labels = zip(*filtered_data_and_labels)
-
-        label_encoder = LabelEncoder()
-        encoded_labels = label_encoder.fit_transform(filtered_labels) 
-        vectors = vectorizer.fit_transform(data_without_stopwords).toarray()
-        #---------------------------------------------------------------#
-
-    #vectors = vectorizer.fit_transform(data_without_stopwords).toarray()
+    # perform train test split given 'test_size'
     return train_test_split(vectors, encoded_labels, test_size=test_size, random_state=42)
-    #return train_test_split(vectors, score, test_size=test_size, random_state=42)
